0facbb38fbfba6540a4b824bca42da8077e92910
FOURIER <webmaster@fourier.jp>
2022-11-09 11:46:45 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -32,39 +32,23 @@ class FrSampleItem extends HTMLElement {
     }
 
     render() {
-        this._root.innerHTML = '<a href="" role="listitem">\n' +
-                               '    <slot></slot>\n' +
-                               '</a>';
-    }
-
-    connectedCallback() {
-        this.render();
-    }
-
-    attributeChangedCallback() {
-        this.render();
-    }
-}
-
-class FrSampleNestItem extends HTMLElement {
-    _root;
-
-    constructor() {
-        super();
-        this._root = this.attachShadow({mode: 'closed'});
-    }
-
-    render() {
-        this._root.innerHTML = '<details role="listitem">\n' +
-                               '    <summary>\n' +
-                               '        <a href="">\n' +
-                               '            <slot></slot>\n' +
-                               '        </a>\n' +
-                               '    </summary>\n' +
-                               '    <div role="list">\n' +
-                               '        <slot name="items"></slot>\n' +
-                               '    </div>\n' +
-                               '</details>';
+        if (this.querySelector('fr-sample-item') === null) {
+            this._root.innerHTML = '<a href="" role="listitem">\n' +
+                                   '    <slot></slot>\n' +
+                                   '</a>';
+        } else {
+                    // <fr-sample-item>があったら動作を変える。
+            this._root.innerHTML = '<details role="listitem">\n' +
+                                   '    <summary>\n' +
+                                   '        <a href="">\n' +
+                                   '            <slot></slot>\n' +
+                                   '        </a>\n' +
+                                   '    </summary>\n' +
+                                   '    <div role="list">\n' +
+                                   '        <slot name="items"></slot>\n' +
+                                   '    </div>\n' +
+                                   '</details>';
+        }
     }
 
     connectedCallback() {
@@ -78,4 +62,3 @@ class FrSampleNestItem extends HTMLElement {
 
 customElements.define('fr-sample', FrSample);
 customElements.define('fr-sample-item', FrSampleItem);
-customElements.define('fr-sample-nest-item', FrSampleNestItem);
